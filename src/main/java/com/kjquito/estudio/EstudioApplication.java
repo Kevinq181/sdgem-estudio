87963bcf16c3412c68868b4c3ae0a18c2c0374ab
Kevin <kevin.kjqm@gmail.com>
2022-07-03 23:55:12 -0500

    pom

--- src/main/java/com/kjquito/estudio/EstudioApplication.java
+++ src/main/java/com/kjquito/estudio/EstudioApplication.java
@@ -2,9 +2,10 @@ package com.kjquito.estudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EstudioApplication {
+public class EstudioApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudioApplication.class, args);
